38b8a3f1a0725f6446378582d3c86003b404ee9b
Giovanni Paolini <giove.paolini@gmail.com>
2015-03-30 12:20:48 +0200

    Aggiunti ordering.

--- words/models.py
+++ words/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Keyword(models.Model):
     keyword = models.CharField(max_length=128, unique=True)
     
+    class Meta:
+        ordering = ('keyword',)
+    
     def __unicode__(self):
         return u'%s' % self.keyword
 
@@ -12,6 +15,9 @@ class Word(models.Model):
     italian = models.CharField(max_length=128)
     
     keywords = models.ManyToManyField(Keyword, blank=True)
+    
+    class Meta:
+        ordering = ('russian', 'italian')
     
     def __unicode__(self):
         return u'%s' % self.russian
